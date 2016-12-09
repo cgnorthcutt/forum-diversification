449b8811fc4882a0af38227667b1163cb0421317
Curtis Northcutt <cgn@mit.edu>
2016-12-08 21:26:44 -0500

    Ipynb numerical run update.

--- gold_data_model_analysis/gold_data_score_tfidf.py
+++ gold_data_model_analysis/gold_data_score_tfidf.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[61]:
 
 from __future__ import absolute_import
 from __future__ import division
@@ -9,7 +9,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 
-# In[2]:
+# In[62]:
 
 import numpy as np
 import pandas as pd
@@ -22,43 +22,14 @@ from scipy.stats import rankdata
 pd.set_option('max_colwidth',5000)
 
 
-# In[3]:
+# In[63]:
 
 # Load word2vec model for this specific course
 w2v_matrix = pickle.load( open( "../data/w2v_matrix_for_HarvardX__HDS_3221_2X__1T2016.p", "rb" ) )
 vocab = np.array(pickle.load( open( "../data/vocab_for_HarvardX__HDS_3221_2X__1T2016.p", "rb" ) ) )
 
 
-# In[4]:
-
-# Verify word2vec model works well (the following example should have cosine similarity near 1)
-
-# def cosinesimilarity(u, v):
-#   # u - embedding (vector)
-#   # v - embedding (vector)
-#   return np.dot(u,v) / (np.sqrt(np.dot(u, u)) * np.sqrt(np.dot(v,v)))
-# np.dot(w2v_matrix[np.where(vocab == "queen")[0][0]], w2v_matrix[np.where(vocab == "king")[0][0]] - w2v_matrix[np.where(vocab == "man")[0][0]] + w2v_matrix[np.where(vocab == "woman")[0][0]] )
-
-
-# In[5]:
-
-test_comments = [
-  "Jesus is great. I love Jesus and the holy book. I think God is amazing. I agree with religion",
-  "Jesus is great. I love Jesus and the holy book. I think God is amazing.",
-  "Jesus is the best. I love Jesus and the holy book. I think God is amazing. I agree with religion",
-  "Jesus is awesome. I love Jesus and the holy book. I think God is amazing. I agree with religion",
-  "Christ is great. I love Christ and the holy book. I think God is amazing. I agree with religion",
-  "Christ is great. I love Christ and the holy book. I agree with religion",
-  "Donald Trump is a president with tons of electrical equipment and lightbulbs and random things.",
-]
-
-
-# In[56]:
-
-TfidfVectorizer(stop_words='english', )
-
-
-# In[57]:
+# In[64]:
 
 def tfidf(gold_data, gold_matrix):
   # Get tfidf counts for each comment as a matrix C with shape (# comments, size of vocab)
@@ -70,7 +41,7 @@ def tfidf(gold_data, gold_matrix):
   return MinMaxScaler().fit_transform(pairwise_cosine_similarity.toarray())
 
 
-# In[58]:
+# In[65]:
 
 # Load gold train data
 gold_matrix_train = pd.read_csv('gold_matrix_train_HarvardX__HDS_3221_2X__1T2016.csv.gz', compression='gzip')
@@ -78,7 +49,7 @@ df_gold_train = pd.read_csv('gold_data_train_HarvardX__HDS_3221_2X__1T2016.csv.g
 pairwise_cosine_similarity_train = tfidf(df_gold_train, gold_matrix_train)
 
 
-# In[59]:
+# In[66]:
 
 # Load gold data
 gold_matrix_test = pd.read_csv('gold_matrix_test_HarvardX__HDS_3221_2X__1T2016.csv.gz', compression='gzip')
@@ -86,21 +57,21 @@ df_gold_test = pd.read_csv('gold_data_test_HarvardX__HDS_3221_2X__1T2016.csv.gz'
 pairwise_cosine_similarity_test = tfidf(df_gold_test, gold_matrix_test)
 
 
-# In[43]:
+# In[67]:
 
 import make_pairwise_gold_metric_scores
 reload(make_pairwise_gold_metric_scores)
 from make_pairwise_gold_metric_scores import compute_metrics
 
 
-# In[60]:
+# In[68]:
 
 metrics = compute_metrics(pairwise_cosine_similarity_train, pairwise_cosine_similarity_test, gold_matrix_train, df_gold_train, gold_matrix_test, df_gold_test)
 pretty_metrics = pd.DataFrame(pd.Series(metrics), columns = ["Score"])
 pretty_metrics
 
 
-# In[45]:
+# In[69]:
 
 # Switching train and test
 
